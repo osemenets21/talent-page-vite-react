8420e2362649a3b081897e0d1cf5d44ea1fb518e
Oleh Semenets <oleg.min.vin@gmail.com>
2025-07-26 11:33:49 -0400

    Revert "talent profile"
    This reverts commit 43e36072ee7d0268607dac3faf3d4b4ec8c8903f.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
 import TalentForm from "./components/TalentForm";
-import SignUpForm from "./components/SignUpForm";
+import SignUpForm from "./components/SignUpForm"; 
+import React from 'react'; 
 import ForgotPassword from "./components/ForgotPassword";
-import TalentProfile from "./components/TalentProfile"; // ← ADD THIS
-import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
@@ -14,22 +12,7 @@ export default function App() {
         <Route path="/" element={<LoginForm />} />
         <Route path="/signup" element={<SignUpForm />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route
-          path="/register-talent"
-          element={
-            <PrivateRoute>
-              <TalentForm />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/talent-profile/:submissionId" // ← SUPPORT FOR PROFILE URL
-          element={
-            <PrivateRoute>
-              <TalentProfile />
-            </PrivateRoute>
-          }
-        />
+        <Route path="/register-talent" element={<TalentForm />} />
       </Routes>
     </Router>
   );

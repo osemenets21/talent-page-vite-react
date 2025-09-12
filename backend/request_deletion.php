52533fa01b93c5f5a44ad5d01dad0d8335b389fa
Oleh Semenets <oleg.min.vin@gmail.com>
2025-09-12 11:37:55 -0400

    test

--- backend/request_deletion.php
+++ backend/request_deletion.php
@@ -1,4 +1,9 @@
 <?php
+// --- DEBUG: Force error display and logging ---
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+error_log("DEBUG: request_deletion.php started");
 declare(strict_types=1);
 
 // ---------- Basic headers ----------

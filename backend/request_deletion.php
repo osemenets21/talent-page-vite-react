dac48036c9206a580b0aca64f478c36d2edc7985
Oleh Semenets <oleg.min.vin@gmail.com>
2025-09-16 19:56:44 -0400

    h

--- backend/request_deletion.php
+++ backend/request_deletion.php
@@ -1,26 +1,28 @@
 <?php
 declare(strict_types=1);
 
-
 require_once __DIR__ . '/env.php';
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 // --- DEBUG: Force error display and logging ---
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-error_log("DEBUG: request_deletion.php started");
+trigger_error('Test error log', E_USER_WARNING); // Remove after confirming errors show up
+
+require_once __DIR__ . '/env.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 // ---------- Basic headers ----------
 header('Content-Type: application/json');
 
-error_reporting(E_ALL);
-ini_set('log_errors', '1');
-$root = rtrim($_SERVER['DOCUMENT_ROOT'], '/');            // e.g. /home/USER/public_html
-$logFile = $root . '/php-error.log';
-if (!file_exists($logFile)) { @touch($logFile); @chmod($logFile, 0644); }
-ini_set('error_log', $logFile);
-error_log("request-deletion start @ " . date('c'));
+// error_reporting(E_ALL);
+// ini_set('log_errors', '1');
+// $root = rtrim($_SERVER['DOCUMENT_ROOT'], '/');            // e.g. /home/USER/public_html
+// $logFile = $root . '/php-error.log';
+// if (!file_exists($logFile)) { @touch($logFile); @chmod($logFile, 0644); }
+// ini_set('error_log', $logFile);
+// error_log("request-deletion start @ " . date('c'));
 
 
 
